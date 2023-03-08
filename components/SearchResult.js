a58e0e83139e9e9854f639cb03f41c4d78e1b857
NyanPham <phamthanhnhanussh@gmail.com>
2023-03-08 17:39:34 +0700

    added html react parser for the description

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,3 +1,5 @@
+import Parser from "html-react-parser"
+
 export default function SearchResult({ results }) {
     return (
         <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -6,8 +8,23 @@ export default function SearchResult({ results }) {
                 in {results.searchInformation.formattedSearchTime} seconds
             </p>
             {results.items.map((result) => (
-                <div className="" key={result.link}>
-                    <p>{result.formattedUrl}</p>
+                <div className="max-w-xl mb-8" key={result.link}>
+                    <div className="group">
+                        <a href={result.link} className="text-sm truncate">
+                            {result.formattedUrl}
+                        </a>
+                        <a
+                            href={result.link}
+                            className="group-hover:underline decoration-blue-800"
+                        >
+                            <h2 className="truncate text-xl font-medium text-blue-800">
+                                {result.title}
+                            </h2>
+                        </a>
+                    </div>
+                    <p className="text-gray-600">
+                        {Parser(result.htmlSnippet)}
+                    </p>
                 </div>
             ))}
         </div>
